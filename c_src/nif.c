5e49d26eb0c3cd229030b8335ba453ef7561b181
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-01-03 08:48:10 +0100

    Remove Nif.sum/2, add Nif.create/1

--- c_src/nif.c
+++ c_src/nif.c
@@ -1,15 +1,42 @@
 #include "erl_nif.h"
 
-ERL_NIF_TERM sum(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+ErlNifResourceType* RESOURCE_TYPE;
+
+typedef struct
 {
-  int a, b;
-  enif_get_int(env, argv[0], &a);
-  enif_get_int(env, argv[1], &b);
-  return enif_make_int(env, a + b);
+  int value;
+} Resource;
+
+static int load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
+{
+  int flags = ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER;
+  RESOURCE_TYPE = enif_open_resource_type(env, "nif", "Resource", NULL, flags, NULL);
+  return 0;
+}
+
+ERL_NIF_TERM create(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  unsigned int a;
+  ERL_NIF_TERM result;
+  Resource* resource;
+
+  if(!enif_get_uint(env, argv[0], &a))
+  {
+    return enif_make_badarg(env);
+  }
+
+  resource = enif_alloc_resource(RESOURCE_TYPE, sizeof(Resource));
+
+  result = enif_make_resource(env, resource);
+  enif_release_resource(resource);
+
+  resource-> value = a;
+
+  return enif_make_tuple2(env, enif_make_atom(env, "ok"), result);
 }
 
 static ErlNifFunc nif_funcs[] = {
-  {"sum", 2, sum}
+  {"create", 1, create}
 };
 
-ERL_NIF_INIT(Elixir.Nif, nif_funcs, NULL, NULL, NULL, NULL);
+ERL_NIF_INIT(Elixir.Nif, nif_funcs, &load, NULL, NULL, NULL);
--- lib/nif.ex
+++ lib/nif.ex
@@ -5,7 +5,7 @@ defmodule Nif do
     :ok = :erlang.load_nif('./priv/nif', 0)
   end
 
-  def sum(_, _) do
+  def create(_) do
     exit(:nif_library_not_loaded)
   end
 end
